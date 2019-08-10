5309da38bbe73df49b4666547cd0c6c0e614bb2e
Joseph Min <kmin@caltech.edu>
2019-08-10 17:15:51 -0400

    move reads table up so that it is more visible (#37)

--- start_local.sh
+++ start_local.sh
@@ -8,4 +8,5 @@ args="$*"
 # docker pull --all-tags lioscro/alaska
 docker-compose -f docker-compose.maintenance.yml down
 cp src/web/assets/js/local_env.js src/web/assets/js/env.js
+docker-compose run --rm mongo mongod --repair
 docker-compose -f docker-compose.yml up $args
